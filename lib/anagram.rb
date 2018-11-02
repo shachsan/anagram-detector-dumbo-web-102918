1532245a01948095cc5d04c5d80f069e553c22e4
SANJAY K SAI <shachsan@hotmail.com>
2018-11-02 21:37:32 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   def match(anagrams)
     anagrams.select do |anagram|
       binding.pry
-      anagram.split.sort == @word.sort
+      anagram.charssort == @word.sort
     end
   end
 end
